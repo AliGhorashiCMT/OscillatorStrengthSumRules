891dfb4ddb657517ba38fc50f47e89d88e6ff6ab
Ali Ghorashi <aligho@mit.edu>
2021-03-30 19:51:37 -0400

    more simulations

--- loopstates.sh
+++ loopstates.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 i=8
-until [ $i -gt 510 ]
+until [ $i -gt 100 ]
 do
   export varname="bandstruct$i"
   export bands=$i
   echo $varname
   mpirun -n 4 jdftx -i Si.nscf.in | tee Si.nscf.$i.out
   echo i: $i
-  ((i=i+20))
+  ((i=i+10))
 done
